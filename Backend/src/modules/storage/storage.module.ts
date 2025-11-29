3654dddd9892495466ebf4cc2eb2b351678a3c65
Anarragan <anbarragana@unal.edu.co>
2025-11-29 11:20:23 -0500

    feat/moduels and entities

--- Backend/src/modules/storage/storage.module.ts
+++ Backend/src/modules/storage/storage.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { StorageService } from './storage.service';
-import { StorageController } from './storage.controller';
+import { StorageController } from '../storage/storage.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Storage } from './entities/storage.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Storage])],
